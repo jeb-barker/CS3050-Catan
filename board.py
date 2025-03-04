f2318dff43846d911ca27564b7680e0c7d1d8a1e
Kai Pasciak <kai.pasciak@gmail.com>
2025-03-04 17:08:37 -0500

    Board

---
+++ board.py
@@ -0,0 +1,14 @@
+'''
+
+'''
+import tile
+class board:
+    def __init__(self):
+        self.players = []
+        self.tiles = []
+        self.vertices = []
+        self.roads = []
+        self.resource_bank = []
+        self.development_cards = []
+
+
