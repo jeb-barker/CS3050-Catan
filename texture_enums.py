3c8acd5b9c3f4acf49a1a9a9189f9ebb78597f5d
Jeb Barker <48389379+jeb-barker@users.noreply.github.com>
2025-03-05 10:22:11 -0500

    Added texture_enums.py - Card enum has values for each type of card (resources/development cards) - Placeholder values for now, but after we upload textures for each card/resource we can use them instead.
    Also added Resource enum
    - TODO: change the board to use these enum values instead of the current string-based list of resources.
    - We should also decide on a standard name to use for resources so these values in the enum are subject to change.

---
+++ texture_enums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+# Values are placeholder for now, they should eventually be a reference to card textures.
+class Card(Enum):
+    # resource cards
+    ore = 0
+    wheat = 1
+    sheep = 2
+    brick = 3
+    wood = 4
+    # development cards
+    knight = 5
+    victory_point = 6
+    road_building = 7
+    year_of_plenty = 8
+    monopoly = 9
+
+# Values are placeholder for now, they should eventually be a reference to a resource hex texture.
+class Resource(Enum):
+    ore = 0
+    wheat = 1
+    sheep = 2
+    brick = 3
+    wood = 5
+    desert = 6
--- tile.py
+++ tile.py
@@ -1,6 +1,5 @@
-import pyglet
-from pyglet.gl import *
 import math
+from texture_enums import *
 
 class Tile:
     """
@@ -12,10 +11,10 @@ class Tile:
     vertices (int[] contains ids of the vertices on this hexagon)
     edges (int[] the index in the edges array)
     """
-    def __init__(self, coords, vertices=[], edges=[], genNum= -1, resource="", resourceTexture= ""):
+    def __init__(self, coords: tuple[int,int], vertices=[], edges=[], genNum= -1, resource=Resource.desert, resourceTexture= ""):
         # Ensure that given coordinate is within the catan board.
-        assert abs(coords.x + coords.y) <= 2
-        
+        assert abs(coords[0] + coords[1]) <= 2
+
         self.coords = coords
         self.neighbors = get_neighbors_from_coordinate(coords)
         self.vertices = vertices
@@ -31,9 +30,11 @@ class Tile:
     
     # TODO: move rendering functions to another file
     # returns a pyglet Polygon
+    """
     def draw_tile(self, x, y, radius) -> pyglet.shapes.Polygon:
         hexagon = pyglet.shapes.Polygon(*self.create_hexagon(x, y, radius), color=(0, 255, 0))
         hexagon.draw()
+    """
     
 # returns the neighbors of an axial coordinate that are within the catan board (hexagonal grid with radius 2).
 def get_neighbors_from_coordinate(coords: tuple[int,int]) -> list[tuple[int,int]]:
@@ -41,9 +42,13 @@ def get_neighbors_from_coordinate(coords: tuple[int,int]) -> list[tuple[int,int]
     # left, up-left, up-right, right, down-right, down-left
     directions = [(-1, 0), (0, -1), (1, -1), (1, 0), (0, 1), (-1, 1)]
     for direction in directions:
-        neighbor_x = coords.x + directions.x
-        neighbor_y = coords.y + directions.y
+        neighbor_x = coords[0] + direction[0]
+        neighbor_y = coords[1] + direction[1]
         # if the new coordinate is within the hexagonal grid...
         if abs(neighbor_x + neighbor_y) <= 2:
             # add the new coordinates to neighbors
             neighbors.append((neighbor_x, neighbor_y))
+    return neighbors
+
+
+print(get_neighbors_from_coordinate((0,0)))
