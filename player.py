212f529d66979f01346e2b0115caae743c31f17b
Kai Pasciak <kai.pasciak@gmail.com>
2025-03-19 16:23:22 -0400

    Player class

--- player.py
+++ player.py
@@ -10,8 +10,17 @@ class Player:
     def __init__(self, player_id, color):
         self.player_id = player_id
         self.color = color
-        self.resource_cards = []
+        self.resources = []
         self.dev_cards = []
 
-   def has_resources(self, type, quantity):
-       pass
+    # Returns boolean based on if player has certain quantity of a given resource
+    def has_resources(self, type, quantity):
+        sum = 0
+        for i in self.resources:
+            if i == type:
+                sum += 1
+
+        if sum >= quantity:
+            return True
+        return False
+
