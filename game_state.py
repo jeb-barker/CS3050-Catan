970190427c8135260404c4686aa6ecb694a5b1ca
Thimbug <bcalhoun@uvm.edu>
2025-03-19 20:36:42 -0400

    Initialized game state w players, turn index, and starting state

--- game_state.py
+++ game_state.py
@@ -5,3 +5,9 @@ class TurnState(Enum):
     AFTER_ROLL = "after_roll"
     BUILDING = "building"
     END_TURN = "end_turn"
+
+class GameState:
+    def __init__(self, players):
+        self.players = players
+        self.current_player_index = 0
+        self.state = TurnState.BEFORE_ROLL
