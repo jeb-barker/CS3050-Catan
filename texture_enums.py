6afd6214aaeef3b1b9192701736282d12baeb0dd
Eli Malloy <elimalloy@icloud.com>
2025-03-26 12:40:34 -0400

    expanded card enum

--- texture_enums.py
+++ texture_enums.py
@@ -10,10 +10,19 @@ class Card(Enum):
     wood = 4
     # development cards
     knight = 5
-    victory_point = 6
-    road_building = 7
-    year_of_plenty = 8
-    monopoly = 9
+    road_building = 6
+    year_of_plenty = 7
+    monopoly = 8
+    # single victory point cards
+    library = 9
+    market = 10
+    chapel = 11
+    university = 12
+    palace = 13
+    # double victory point cards
+    longest_road = 14
+    largest_army = 15
+
 
 # Values are placeholder for now, they should eventually be a reference to a resource hex texture.
 class Resource(Enum):
