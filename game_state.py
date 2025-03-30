ef5f5fde964755f9dd3d7e387480ab743aaef5ee
Thimbug <bcalhoun@uvm.edu>
2025-03-30 14:03:10 -0400

    pulled game_state.py to basic_interactivity branch

--- game_state.py
+++ game_state.py
@@ -11,3 +11,41 @@ class GameState:
         self.players = players
         self.current_player_index = 0
         self.state = TurnState.BEFORE_ROLL
+
+    def get_current_player(self):
+        """Returns the player whose turn it is."""
+        return self.players[self.current_player_index]
+
+    def roll_dice(self):
+        """Transition from BEFORE_ROLL to AFTER_ROLL."""
+        if self.state == TurnState.BEFORE_ROLL:
+            self.state = TurnState.AFTER_ROLL
+            return True
+        return False
+
+    def start_building_phase(self):
+        """Transition from AFTER_ROLL to BUILDING."""
+        if self.state == TurnState.AFTER_ROLL:
+            self.state = TurnState.BUILDING
+            return True
+        return False
+
+    def end_turn(self):
+        """End the turn and move to the next player."""
+        if self.state == TurnState.BUILDING:
+            self.next_turn()
+            return True
+        return False
+
+    def next_turn(self):
+        """Cycle to the next player's turn."""
+        self.current_player_index = (self.current_player_index + 1) % len(self.players)
+        self.state = TurnState.BEFORE_ROLL
+
+    def get_ui_state(self):
+        """Returns which buttons should be active."""
+        return {
+            "roll_dice": self.state == TurnState.BEFORE_ROLL,
+            "build": self.state == TurnState.AFTER_ROLL,
+            "end_turn": self.state == TurnState.BUILDING
+        }
